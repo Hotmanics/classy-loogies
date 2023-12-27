87424c979ebdedb5f03fd977d1748a182776a98a
Jacob Homanics <homanicsjake@gmail.com>
2023-12-27 04:52:53 -0600

    trying to fix linting

--- packages/nextjs/pages/classy-loogies.tsx
+++ packages/nextjs/pages/classy-loogies.tsx
@@ -3,14 +3,14 @@ import { MetaHeader } from "~~/components/MetaHeader";
 import { ClassyLoogies } from "~~/components/classy-loogies/classy-loogies";
 
 const ClassyLoogiesPage: NextPage = () => {
-    return (
-        <>
-            <MetaHeader title="Classy Loogies" description="Classy Loogies" />
-            <div className="grid lg:grid-cols-1 flex-grow" data-theme="exampleUi">
-                <ClassyLoogies />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <MetaHeader title="Classy Loogies" description="Classy Loogies" />
+      <div className="grid lg:grid-cols-1 flex-grow" data-theme="exampleUi">
+        <ClassyLoogies />
+      </div>
+    </>
+  );
 };
 
 export default ClassyLoogiesPage;
